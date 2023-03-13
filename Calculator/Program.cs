9d65b0e68f2e48399477f3db05f9cf9217cba819
Jack Sanders <jack_sanders@hotmail.co.uk>
2023-03-13 14:38:24 +0000

    renamed variables within the switch to reduce overuse of new variables

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -17,91 +17,95 @@ namespace Calculator
                 Console.WriteLine("6. Roots");
                 Console.WriteLine();
                 Console.WriteLine("7. Exit");
+               
 
                 int function = Convert.ToInt32(Console.ReadLine());
                 switch (function)
-                {
+                {   
+                     
                     case 1:
+                        
                         Console.WriteLine();
                         Console.WriteLine("Please enter the first number:");
-                        int addFirst = Convert.ToInt32(Console.ReadLine());
+                        int numOne = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("+");
-                        int addSecond = Convert.ToInt32(Console.ReadLine());
+                        int numTwo = Convert.ToInt32(Console.ReadLine());
 
-                        int addTotal = addFirst + addSecond;
+                        double product = numOne + numTwo;
                         Console.WriteLine();
                         Console.WriteLine("=");
-                        Console.WriteLine(addTotal);
+                        Console.WriteLine(product);
                         Console.WriteLine();
                         break;
-
+                        
                     case 2:
+                        
                         Console.WriteLine();
                         Console.WriteLine("Enter the first number:");
-                        int subFirst = Convert.ToInt32(Console.ReadLine());
+                        numOne = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("-");
-                        int subSecond = Convert.ToInt32(Console.ReadLine());
+                        numTwo = Convert.ToInt32(Console.ReadLine());
 
-                        int subTotal = subFirst - subSecond;
+                        product = numOne - numTwo;
                         Console.WriteLine();
                         Console.WriteLine("=");
-                        Console.WriteLine(subTotal);
+                        Console.WriteLine(product);
                         Console.WriteLine();
                         break;
-
+                        
                     case 3:
                         Console.WriteLine();
                         Console.WriteLine("Enter the first number:");
-                        int multiFirst = Convert.ToInt32(Console.ReadLine());
+                        numOne = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("*");
-                        int multiSecond = Convert.ToInt32(Console.ReadLine());
+                        numTwo = Convert.ToInt32(Console.ReadLine());
 
-                        int multiTotal = multiFirst * multiSecond;
+                        product = numOne * numTwo;
                         Console.WriteLine();
                         Console.WriteLine("=");
-                        Console.WriteLine(multiTotal);
+                        Console.WriteLine(product);
                         Console.WriteLine();
                         break;
 
                     case 4:
                         Console.WriteLine();
                         Console.WriteLine("Enter the first number:");
-                        int divFirst = Convert.ToInt32(Console.ReadLine());
+                        numOne = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("÷");
-                        int divSecond = Convert.ToInt32(Console.ReadLine());
+                        numTwo = Convert.ToInt32(Console.ReadLine());
 
-                        int divTotal = divFirst / divSecond;
+                        product = numOne / numTwo;
                         Console.WriteLine();
                         Console.WriteLine("=");
-                        Console.WriteLine(divTotal);
+                        Console.WriteLine(product);
                         Console.WriteLine();
                         break;
 
                     case 5:
                         Console.WriteLine();
                         Console.WriteLine("Enter the number you want to raise:");
-                        double powerFirst = Convert.ToInt32(Console.ReadLine());
+                        numOne = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("Enter the power");
-                        double powerSecond = Convert.ToInt32(Console.ReadLine());
+                        numTwo = Convert.ToInt32(Console.ReadLine());
 
-                        double powerTotal = Math.Pow(powerFirst, powerSecond);
+                        product = Math.Pow(numOne, numTwo);
                         Console.WriteLine();
                         Console.WriteLine("=");
-                        Console.WriteLine(powerTotal);
+                        Console.WriteLine(product);
                         Console.WriteLine();
                         break;
 
                     case 6:
                         Console.WriteLine();
                         Console.WriteLine("Enter the number you want to find the root of:");
-                        double rootFirst = Convert.ToInt32(Console.ReadLine());
+                         numOne = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("Enter the root number");
-                        double rootSecond = Convert.ToInt32(Console.ReadLine());
+                         numTwo = Convert.ToInt32(Console.ReadLine());
 
-                        double rootTotal = Math.Pow(rootFirst, 1.0 / rootSecond);
+                         product = Math.Pow(numOne, 1.0 / numTwo);
                         Console.WriteLine();
                         Console.WriteLine("=");
-                        Console.WriteLine(rootTotal);
+                        Console.WriteLine(product);
                         Console.WriteLine();
                         break;
 
